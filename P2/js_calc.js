effd95fe0be62e16843a6e57beaa94ecce88d6cc
algarmu <a.garciamu.2018@alumnos.urjc.es>
2021-05-05 20:00:03 +0200

    implementacion calcfunciones basicas terminada

--- P2/js_calc.js
+++ P2/js_calc.js
@@ -17,7 +17,8 @@ const ESTADO = {
 
 let digitos = document.getElementsByClassName("num");
 let operacion = document.getElementsByClassName("operacion");
-let estado = ESTADO.INIT;   
+let estado = ESTADO.INIT; 
+let estado_log = [];  
 
 for(i=0; i<digitos.length; i++){
   digitos[i].onclick=(ev)=>{
@@ -25,48 +26,52 @@ for(i=0; i<digitos.length; i++){
   }
 }
 
+//bucle de las operaciones
 for(i=0; i<operacion.length; i++){
     operacion[i].onclick = (ev) =>{
       if(estado == ESTADO.OP1){
         display.innerHTML += ev.target.value;
         estado_log.push(estado);
         estado = ESTADO.OPERATION;
-        console.log(estado, "calculo");
-        
+        console.log(estado, "calculo"); 
       }
     }
-  }
+}
 
-  clear.onclick = () => {
+//implementacion del reset
+clear.onclick = () => {
     display.innerHTML = "0";
     estado = ESTADO.INIT;
     console.log(`ESTADO ${estado}`);
     ESTADO.COMA = false;
-  }
+}
 
+//implementacion de la raiz
 sqrt.onclick = () => {
     display.innerHTML = Math.sqrt(display.innerHTML);
-  }
-  
+}
+
+//implementacion del igual
 igual.onclick = () => {
     display.innerHTML = eval(display.innerHTML);
     estado = ESTADO.OP1;
     console.log(`ESTADO ${estado}`);
     ESTADO.COMA = true;
-  }
+}
 
+//implementacion del delete
 del.onclick = () => {
     if((display.innerHTML == '0')||(display.innerHTML == '')){
       display.innerHTML = '0';
     }else {
       display.innerHTML = display.innerHTML.slice(0,-1);
     }
-  }
+}
 
 
 function digito(botones)
 {
-  if(estado == ESTADO.INIT){
+    if(estado == ESTADO.INIT){
     display.innerHTML = botones;
     estado = ESTADO.OP1;
     console.log(`ESTADO ${estado}`);
@@ -85,4 +90,4 @@ function operaciones(operacion){
       display.innerHTML += operacion;
       estado = ESTADO.OPERATION;
     }
-  }
+}
