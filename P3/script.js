7a644de37b38b2f77b0b3d8ae53536fc302f7722
algarmu <a.garciamu.2018@alumnos.urjc.es>
2021-05-17 22:41:58 +0200

    js breakout mas cambios

--- P3/script.js
+++ P3/script.js
@@ -41,6 +41,11 @@ const brickInfo = {
 const bricks = [];
 for (let i = 0; i < brickRowCount; i++) {
   bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
 }
 
 // bola en el canvas
