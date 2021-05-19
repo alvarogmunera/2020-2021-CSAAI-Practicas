c8588bac66a08a5c6e8329a22053b6d7bb65ca02
algarmu <a.garciamu.2018@alumnos.urjc.es>
2021-05-19 22:15:15 +0200

    cambios js P4

--- P4/script.js
+++ P4/script.js
@@ -16,3 +16,20 @@ const range_value_green = document.getElementById('range_value_green');
 
 const grises = document.getElementById('gris');
 const colors = document.getElementById('color');
+//-- Función de retrollamada de imagen cargada
+//-- La imagen no se carga instantaneamente, sino que
+//-- lleva un tiempo. Sólo podemos acceder a ella una vez
+//-- que esté totalmente cargada
+img.onload = function () {
+
+    //-- Se establece como tamaño del canvas el mismo
+    //-- que el de la imagen original
+    canvas.width = img.width;
+    canvas.height = img.height;
+  
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0);
+  
+    console.log("Imagen lista...");
+  };
